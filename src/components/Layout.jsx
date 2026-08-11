6b036ccdc909f9e404d5c40ed214ec7ac84dc829
Nahush Patel <113813171+deviljitu1@users.noreply.github.com>
2026-08-11 16:55:41 +0530

    Add floating action buttons and footer social links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,15 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 
+import FloatingActionButtons from './FloatingActionButtons'
+
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
       <Footer />
+      <FloatingActionButtons />
     </>
   )
 }
